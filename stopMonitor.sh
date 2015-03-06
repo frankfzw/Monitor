1a6c8ac273166ac38677916bc3ebd5bdc38da2ad
frankfzw <frankfzw@sjtu.edu.cn>
2015-03-06 14:47:05 +0800

    add funtion of scp data from slave to master:

--- stopMonitor.sh
+++ stopMonitor.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 PASSWORD=$1 
-#MONITOR_HOME=$2
+MONITOR_HOME=$2
 
-usage="stopMonitor Password"
+usage="stopMonitor Password MonitorHome"
 if [ $# -le 0 ]; then
   echo $usage
   exit 1
 fi
 # stop the monitor on the master at first
 echo $1 | sudo -S screen -X -S monitor quit
+mv $MONITOR_HOME/in $MONITOR_HOME/master_in
+mv $MONITOR_HOME/out $MONITOR_HOME/master_out
 
 # Where to start the script, see hadoop-config.sh
 # (it set up the variables based on command line options)
@@ -23,6 +25,12 @@ fi
 # Kill the monitor on slaves
 for slave in $SLAVE_NAMES ; do
  echo $1 | ssh hduser@$slave sudo -S screen -X -S monitor quit
+
+ # copy traffic files from slaves
+ scp hduser@$slave:$MONITOR_HOME/in $MONITOR_HOME
+ mv $MONITOR_HOME/in $slave+"_in"
+ scp hduser@$slave:$MONITOR_HOME/out $MONITOR_HOME
+ mv $MONITOR_HOME/out $slave+"_out"
  if [ "$HADOOP_SLAVE_SLEEP" != "" ]; then
    sleep $HADOOP_SLAVE_SLEEP
  fi
